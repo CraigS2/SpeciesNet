394bec0e0eb497ed2b37e638ae20d188c3ad253f
James Curtin <jameswcurtin@gmail.com>
2026-07-21 00:36:41 -0400

    Fix reverse() URL name in rejectSpeciesImportBatch

--- speciesnet/species/views/views_species_import.py
+++ speciesnet/species/views/views_species_import.py
@@ -220,7 +220,7 @@ def rejectSpeciesImportBatch(request, pk):
         logger.info('User %s bulk-rejected %d staging records for archive %s', request.user.username, count, pk)
         messages.success(request, f'{count} staging record(s) rejected.')
 
-    return HttpResponseRedirect(reverse('species/import/reviewSpeciesImport', args=[pk]))
+    return HttpResponseRedirect(reverse('reviewSpeciesImport', args=[pk]))
 
 
 # ---------------------------------------------------------------------------
